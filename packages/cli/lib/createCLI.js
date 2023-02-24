75c94c86333ba5744352d6133ee8da7119c003b3
叶继伟 <yejw6@asiainfo.com>
2023-02-24 16:48:57 +0800

    feat: 脚手架注册+命令注册

---
+++ packages/cli/lib/createCLI.js
@@ -0,0 +1,31 @@
+import fse from "fs-extra";
+import path from "path";
+import { dirname } from "dirname-filename-esm";
+
+const __dirname = dirname(import.meta);
+const pkgPath = path.resolve(__dirname, "../package.json");
+const pkg = fse.readJsonSync(pkgPath);
+
+export default function createCLI(program) {
+
+  // 命令注册
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage("<command> [options]")
+    .version(pkg.version, "-v, --version", "输出当前版本号")
+    .option("-d, --debug", "是否开启调试模式", false)
+    .helpOption("-h, --help", "命令显示帮助")
+    .addHelpCommand("help [command]", "命令显示帮助");
+
+  // 监听未知的命令
+  program.on("command:*", function (obj) {
+    console.error("未知的命令：" + obj[0]);
+  });
+
+  // 监听 debug 选项
+  program.on("option:debug", function () {
+    if (program.opts().debug) {
+      console.log("开启调试模式");
+    }
+  });
+}
--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -1,3 +1,9 @@
+import { program } from "commander";
+import createCLI from "./createCLI.js";
+
 export default function (args) {
-  console.log("this is entry", args);
+  createCLI(program);
+
+  // 解析配置
+  program.parse(process.argv);
 }
