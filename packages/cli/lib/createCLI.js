ba835cd1e3c5fa96b465861d5604dd6a80ac0f6a
叶继伟 <yejw6@asiainfo.com>
2023-02-24 18:23:06 +0800

    feat: Node 最低版本检查功能

--- packages/cli/lib/createCLI.js
+++ packages/cli/lib/createCLI.js
@@ -2,11 +2,28 @@ import fse from "fs-extra";
 import path from "path";
 import { dirname } from "dirname-filename-esm";
 import { log } from "@coderyjw/utils";
+import semver from "semver";
+import chalk from "chalk";
 
 const __dirname = dirname(import.meta);
 const pkgPath = path.resolve(__dirname, "../package.json");
 const pkg = fse.readJsonSync(pkgPath);
+const LOWEST_NODE_VERSION = "14.0.0";
 
+const checkNodeVersion = () => {
+  log.verbose("node version", process.version);
+  // 当前 node 版本低于 LOWEST_NODE_VERSION 时 抛出错误
+  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
+    throw new Error(
+      chalk.red(`jw-cli 需要安装 ${LOWEST_NODE_VERSION} 以上版本的 Node.js`)
+    );
+  }
+};
+
+const preAction = () => {
+  // 检查 Node 版本
+  checkNodeVersion();
+};
 export default function createCLI(program) {
   // 命令注册
   program
@@ -15,7 +32,8 @@ export default function createCLI(program) {
     .version(pkg.version, "-v, --version", "输出当前版本号")
     .option("-d, --debug", "是否开启调试模式", false)
     .helpOption("-h, --help", "命令显示帮助")
-    .addHelpCommand("help [command]", "命令显示帮助");
+    .addHelpCommand("help [command]", "命令显示帮助")
+    .hook("preAction", preAction);
 
   // 监听未知的命令
   program.on("command:*", function (obj) {
